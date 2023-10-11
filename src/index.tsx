f6d404d9bdd3b0f54914fcc22f6a33ffb79446b5
RDestrez <raphael.destrez@gmail.com>
2023-10-11 16:23:47 +0200

    initialisation router

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App/App';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App/App";
+import { Provider } from "react-redux";
 //import reportWebVitals from './reportWebVitals';
-import {store} from './App/store/store';
+import { store } from "./App/store/store";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Provider store={store} >
-    <App />
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
     </Provider>
   </React.StrictMode>
 );
