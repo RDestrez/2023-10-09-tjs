981dd7dd0d97873a55911129d42076e3dde3be11
RDestrez <raphael.destrez@gmail.com>
2023-10-11 17:35:11 +0200

    fin route

--- src/App/components/pages/Editor.jsx
+++ src/App/components/pages/Editor.jsx
@@ -5,12 +5,14 @@ import { ConnectedMemeForm } from '../functionnals/MemeForm/MemeForm';
 import {useParams} from 'react-router-dom';
 import {useDispatch, useSelector} from "react-redux";
 import { changeMeme, clearMeme } from '../../store/currentSlice';
+import {useNavigate} from 'react-router-dom'
 
 
 export default function Editor() {
   const params=useParams();
   const d= useDispatch();
   const memes=useSelector(s=>s.ressources.memes);
+  const navigate = useNavigate();
 
   useEffect(() => {
     return () => {
@@ -21,13 +23,14 @@ export default function Editor() {
         const current=memes.find((m) => m.id === Number(params.id));
         if(undefined !== current){
           d(changeMeme());
-        }else{
+        }else if(memes.length ===0){
+          navigate.push('/editor')
           // pas de meme avec l'id present dans les param
         }
       
       }
     };
-  }, [])
+  }, [memes,params,d,navigate])
   return (
     <>
           <MemeSvgViewer basePath="" />
